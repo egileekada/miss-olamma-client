ac15d6327da87af5dcb437703b6932c92627f875
George, B. Smith <georgebsmith.tech@gmail.com>
2022-02-16 22:06:04 +0100

    added the design for about, vision and misssion

--- src/pages/index.js
+++ src/pages/index.js
@@ -93,7 +93,111 @@ export default function Home() {
         </div>
       </header>
 
-      <main className={styles.main}></main>
+      <main>
+        <section
+          style={{
+            padding: "53px 30px",
+            backgroundColor: "rgba(5, 7, 5, 0.8)",
+          }}
+        >
+          <div style={{ marginBottom: 105 }}>
+            <h2
+              style={{
+                textAlign: "center",
+                color: "rgba(255, 255, 255, 1)",
+                fontSize: 22,
+                fontWeight: "700",
+                fontFamily: "Circular Std",
+              }}
+            >
+              About us
+            </h2>
+            <p
+              style={{
+                lineHeight: 1.4,
+                color: "rgba(238, 238, 238, 1)",
+                fontFamily: 14,
+              }}
+            >
+              Miss Olamma Africa is not only a celebration of the beauty of
+              Africa from the eyes of the Igbo women of South East Nigeria, it
+              is the acknowledgement of the most virtuous, most valuable, the
+              most precious gift of all that the universe has bestowed upon
+              humanity, THE WOMAN.
+            </p>
+          </div>
+          <div style={{ marginBottom: 30 }}>
+            <h2
+              style={{
+                color: "rgba(255, 255, 255, 0.9)",
+                fontSize: 20,
+                fontWeight: "500",
+                fontFamily: "Circular Std",
+                marginBottom: 17.3,
+              }}
+            >
+              Our vision
+            </h2>
+            <p
+              style={{
+                lineHeight: 1.4,
+                color: "rgba(226, 226, 226, 0.9)",
+                fontFamily: 16,
+              }}
+            >
+              We are determined to create a pan African platform that celebrates
+              the African beauty through the eyes of the Igbo Woman.
+            </p>
+          </div>
+          <div style={{ marginTop: 30 }}>
+            <h2
+              style={{
+                color: "rgba(255, 255, 255, 0.9)",
+                fontSize: 20,
+                fontWeight: "500",
+                fontFamily: "Circular Std",
+                marginBottom: 17.3,
+              }}
+            >
+              Our mission
+            </h2>
+            <p
+              style={{
+                lineHeight: 1.4,
+                color: "rgba(226, 226, 226, 0.9)",
+                fontFamily: 16,
+              }}
+            >
+              To design an annual contest that allows Igbo African Women to
+              express themselves through pageantry.
+            </p>
+          </div>
+          <div style={{ marginTop: 30 }}>
+            <h2
+              style={{
+                color: "rgba(255, 255, 255, 0.9)",
+                fontSize: 20,
+                fontWeight: "500",
+                fontFamily: "Circular Std",
+                marginBottom: 17.3,
+              }}
+            >
+              The Contest
+            </h2>
+            <p
+              style={{
+                lineHeight: 1.4,
+                color: "rgba(226, 226, 226, 0.9)",
+                fontFamily: 16,
+              }}
+            >
+              Miss Olamma Africa Pageant is an online voting contest that
+              culminates into a grand finale amidst a high-quality audience, the
+              media and stakeholders across Nigeria
+            </p>
+          </div>
+        </section>
+      </main>
 
       <footer className={styles.footer}>
         <a
